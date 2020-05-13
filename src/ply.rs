b11804aee52adebea38cbafa86ebb31c00fdd138
Ryan Norris <rynorris@gmail.com>
2020-05-13 17:31:15 +0900

    Print out model bounds when loading

--- src/ply.rs
+++ src/ply.rs
@@ -101,12 +101,28 @@ pub fn load_ply_file(filename: &str) -> Model {
     let ply_vertices = &ply.payload["vertex"];
     let ply_faces = &ply.payload["face"];
 
+    let mut min_x = f64::MAX;
+    let mut min_y = f64::MAX;
+    let mut min_z = f64::MAX;
+    let mut max_x = f64::MIN;
+    let mut max_y = f64::MIN;
+    let mut max_z = f64::MIN;
+
     let vertices: Vec<Vector3> = ply_vertices.iter().map(|v| {
-        Vector3{
+        let v = Vector3{
             x: v.get_float(&vertex.properties["x"].name).unwrap() as f64,
             y: v.get_float(&vertex.properties["y"].name).unwrap() as f64,
             z: v.get_float(&vertex.properties["z"].name).unwrap() as f64,
-        }
+        };
+
+        min_x = f64::min(min_x, v.x);
+        min_y = f64::min(min_y, v.y);
+        min_z = f64::min(min_z, v.z);
+        max_x = f64::max(max_x, v.x);
+        max_y = f64::max(max_y, v.y);
+        max_z = f64::max(max_z, v.z);
+
+        v
     }).collect();
 
     let faces: Vec<(usize, usize, usize)> = ply_faces.iter().map(|f| {
@@ -115,6 +131,7 @@ pub fn load_ply_file(filename: &str) -> Model {
     }).collect();
 
     println!("Loaded {} vertices and {} faces.", vertices.len(), faces.len());
+    println!("Model bounds: X: {}-{}, Y: {}-{}, Z: {}-{}", min_x, max_x, min_y, max_y, min_z, max_z);
 
     Model{ vertices, faces }
 }
